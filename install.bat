dd21a719856c7c01e62817e18fcd1f4ce7b1d6b9
theonlypeti <boothiepro@gmail.com>
2023-07-21 02:33:13 +0200

    reduced requirements install script updated

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
 python.exe -m pip install --upgrade pip setuptools wheel
 python.exe -m pip install -r requirements.txt
+if not exist ".\credentials" mkdir .\credentials
+@echo MAIN_DC_TOKEN=ABC> credentials\main.env
 python main_bot.py --debug
